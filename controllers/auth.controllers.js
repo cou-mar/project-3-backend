5d17cb4028c0dd5a80b7865ee5254715415fe5dc
Courtney Marasigan <coumardev@gmail.com>
2022-12-19 16:20:28 -0500

    login auth token

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -35,7 +35,59 @@ const signupController = (req, res, next) => {
 
 const loginController = (req, res, next) => {
     // res.send('login route works');
-}
+
+    //destructuring
+    const {email, password} = req.body;
+
+    //guard statement
+    if(!email || !password){
+        return res.json({
+            error: {
+                message: 'Missing email or password. Please fill out each field before continuing.'
+            }
+        })
+    }
+        //testing whether guard statement works in postman
+        //res.send(req.body);
+
+    let myUser;
+
+    //checking if user exists when attempting to log in
+    User.findOne({email: email})
+        .then(foundUser => {
+            // res.json(foundUser);
+
+            if(!foundUser){
+                return Promise.reject('Invalid email or password.')
+            }
+            myUser = foundUser;
+            return bcryptjs.compare(password, foundUser.password)
+        })
+        .then(isValidPassword => {
+            // res.send(isValidPassword)
+            if(!isValidPassword){
+                return Promise.reject('Invalid email or password.')
+            }
+            //issuing token
+            const payload = {
+                _id: myUser._id,
+                name: myUser.name,
+                email: myUser.email
+            };
+            const authToken = jwt.sign(
+                payload,
+                process.env.TOKEN_SECRET,
+                {
+                    algorithm: 'HS256',
+                    expiresIn: '120h'
+                }
+            );
+            res.json({
+                authToken: authToken
+            });
+        })
+        .catch(err => res.send(err))
+};
 
 module.exports = { 
     signupController,
